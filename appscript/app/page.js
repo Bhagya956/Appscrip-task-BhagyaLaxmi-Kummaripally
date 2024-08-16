43d21b8300959085917703149210f8935589bf04
unknown <k.bhagya956@gmail.com>
2024-08-17 00:02:54 +0530

    added files

---
+++ appscript/app/page.js
@@ -0,0 +1,33 @@
+"use client"
+import React from 'react'
+
+
+import Header from './Components/Header.js'
+import Body from './Components/Body'
+
+import Hidefilters from './Components/Hidefilters'
+import Footer from './Components/Footer'
+
+
+
+const page = () => {
+ 
+  return (
+    <div>
+     <Header/>
+
+     <div style={{display:"flex", gap: "170px" }} >
+     <Hidefilters/>
+     <Body/>
+     </div>
+     <Footer/>
+   
+
+   
+    
+     
+    </div>
+  )
+}
+
+export default page
